eeac7c2e10b3283338d88b7670e2fb02f66ff26d
Alfonso Lizárraga <alfonso@nextortelecom.com>
2013-07-12 11:21:05 -0500

    Version 2 del script de instalacion

--- Pyinstall.sh
+++ Pyinstall.sh
@@ -1,27 +1,25 @@
+#!/usr/bin/env bash
+
 #Installing Python 2.7.5 on Elastix 1.6
-
-    wget http://python.org/ftp/python/2.7.5/Python-2.7.5.tar.bz2
-    tar xf Python-2.7.5.tar.bz2
-    cd Python-2.7.5
-    ./configure --prefix=/usr/local
-    make && make altinstall
-
-
+wget http://python.org/ftp/python/2.7.5/Python-2.7.5.tar.bz2
+tar xf Python-2.7.5.tar.bz2
+cd Python-2.7.5
+./configure --prefix=/usr/local
+make && make altinstall
 
 #Installing Distribute
-
-
-    wget http://pypi.python.org/packages/source/d/distribute/distribute-0.6.35.tar.gz
-    tar xf distribute-0.6.35.tar.gz
-    cd distribute-0.6.35
-    python2.7 setup.py install
-
-
+wget http://pypi.python.org/packages/source/d/distribute/distribute-0.6.35.tar.gz
+tar xf distribute-0.6.35.tar.gz
+cd distribute-0.6.35
+python2.7 setup.py install
 
 #Installing virtualenv and pip
+easy_install virtualenv pip
 
-
-    easy_install-2.7 virtualenv pip
+#Creating virtualenv directory and installation paths
+mkdir /opt/NEXTOR/tarificador && /opt/NEXTOR/tarificador
+virtualenv env
+source env/bin/activate
 
 #Installing django
-    sudo pip install django
+pip install django
