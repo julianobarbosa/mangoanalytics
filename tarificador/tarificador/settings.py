25336ad1f7ca6f992c2ac0c337d529ac7b99a038
Luis Lizarraga <siul.al4@gmail.com>
2013-07-15 17:39:30 -0500

    Formularios terminados

--- tarificador/tarificador/settings.py
+++ tarificador/tarificador/settings.py
@@ -72,7 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ("static", "/opt/NEXTOR/tarificador/django-tarificador/tarificador/tarifica/static/tarifica")
+    ("bootstrap", "/opt/NEXTOR/tarificador/django-tarificador/tarificador/tarifica/static/tarifica/bootstrap")
 )
 
 # List of finder classes that know how to find static files in
