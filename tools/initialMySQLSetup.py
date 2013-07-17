d80d73bd507bd746469f48c405f1e2d90fa0b1bb
Alfonso Lizárraga <alfonso@nextortelecom.com>
2013-07-17 12:47:34 -0500

    adding init mysql script

--- tools/initialMySQLSetup.py
+++ tools/initialMySQLSetup.py
@@ -1,15 +1,45 @@
 #!/usr/bin/env python2.7
 import MySQLdb
-import AsteriskMySQLManager
 
-class CallInformationExtracter:
-	def getDailyCallInformation(self):
-		am = AsteriskMySQLManager()
-		am.connect()
-		am.cursor.execute('SELECT SUM(real_duration), extension, uniqueId, date \
-			FROM calls \
-			WHERE status = %s AND action = %s AND date < %s AND date > %s \
-			GROUP BY extension', ('ANSWERED', 'Dial', 'tomorrow', 'yesterday'))
+def getMySQLPassword():
+	elastixConf = open('/etc/elastix.conf', 'r')
+	while True:
+		line = elastixConf.readline()
+		if not line: 
+			break
+		if 'mysqlrootpwd' in line:
+			line.strip('\n')
+			return line.split('=')[1]
 
-	def getDailyDestinationInformation(self):
-		pass
+# 1 - Obtain mysql root password:
+$rootPass = getMySQLPassword()
+print rootPass
+
+dbName = 'nextor_tarificador'
+dbUser = 'nxt_tarificador'
+dbPass = 'k4590NAEUI'
+
+# 2 - Connect to mysql:
+db = MySQLdb.connect(host='localhost',user='root',
+	passwd=rootPass,db='mysql')
+cursor = db.cursor
+
+# 3 - Create database:
+createDBSQL = 'CREATE DATABASE %s'
+cursor.execute(createDBSQL, (dbName,))
+
+# 4 - Create user:
+createUserSQL = "CREATE USER %s@'localhost' IDENTIFIED BY %s"
+cursor.execute(createUserSQL, (dbUser, dbPass))
+
+# 5 - Assign privileges for own db
+assignPriv = "GRANT ALL PRIVILEGES ON %s.* TO %s@'localhost'"
+cursor.execute(assignPriv, (dbName, dbUser))
+
+# 6 - Assign privileges for asterisk main db
+asteriskPriv = "GRANT SELECT ON asterisk.* TO %s@'localhost'"
+cursor.execute(asteriskPriv, (dbUser,))
+
+# 6 - Assign privileges for asterisk cdr db
+asteriskPriv = "GRANT SELECT ON asteriskcdrdb.* TO %s@'localhost'"
+cursor.execute(asteriskPriv, (dbUser,))
