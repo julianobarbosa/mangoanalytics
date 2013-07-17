445e5d840c3aed1406147b6267ea5c6c89a0bf6b
Luis Lizarraga <siul.al4@gmail.com>
2013-07-17 16:35:03 -0500

    Terminando Troncales

--- tarificador/tarifica/urls.py
+++ tarificador/tarifica/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import patterns, url
 from tarifica import views
 
 urlpatterns = patterns('',
-   url(r'^setuptroncales/(?P<asterisk_id>\d+)$', views.setupAddProviderInfo, name = 'Agregar informacion de troncal'),
-   url(r'^setuptarifabase/(?P<asterisk_id>\d+)$', views.setupAddBaseTariffs, name = 'Agregar las tarifas base'),
-   url(r'^setuppaquetes/(?P<asterisk_id>\d+)$', views.setupAddBundles, name = 'Agregar paquetes'),
+   url(r'^setuptroncales/(?P<asterisk_id>\d+)$', views.setupAddProviderInfo, name = 'setuptroncales'),
+   url(r'^setuptarifabase/(?P<asterisk_id>\d+)$', views.setupAddBaseTariffs, name = 'setuptarifabase'),
+   url(r'^setuppaquetes/(?P<asterisk_id>\d+)$', views.setupAddBundles, name = 'setuppaquetes'),
    url(r'^thanks$', views.thanks, name = 'gracias'),
    url(r'^contact$',views.thanks, name = 'contacto'),
-   url(r'^dashboardtroncales$', views.dashboardTrunks, name = 'Dashboard Troncales')
+   url(r'^dashboardtroncales$', views.dashboardTrunks, name = 'dashboardtroncales')
 )
