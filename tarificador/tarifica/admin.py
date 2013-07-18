a5b5c6b8e2c8dedfd597924b74a55be016cd988c
Luis Lizarraga <siul.al4@gmail.com>
2013-07-18 12:32:24 -0500

    Terminando Troncales

--- tarificador/tarifica/admin.py
+++ tarificador/tarifica/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from tarifica.models import Provider, PaymentType, TariffMode
+from tarifica.models import Provider, PaymentType, TariffMode, BaseTariff, DestinationGroup
 
 admin.site.register(Provider)
 admin.site.register(PaymentType)
 admin.site.register(TariffMode)
+admin.site.register(BaseTariff)
+admin.site.register(DestinationGroup)
