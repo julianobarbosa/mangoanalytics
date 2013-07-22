e74f5c60a905e46bd3488a02f6fabe34321d48f8
Luis Lizarraga <siul.al4@gmail.com>
2013-07-22 11:29:51 -0500

    ayayay

--- tarificador/tarifica/views.py
+++ tarificador/tarifica/views.py
@@ -91,7 +91,7 @@ def dashboardTrunks(request):
     a_mysql_m = AsteriskMySQLManager()
     trunks = a_mysql_m.getTrunkInformation()
     for x in trunks:
-        if x:
+        if x['trunkid']:
             try:
                 e = Provider.objects.get(asterisk_id = x['trunkid'])
             except Provider.DoesNotExist:
