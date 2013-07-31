ae1a426be3fbd769f243de2d959c826e2114e7ac
Luis Lizarraga <siul.al4@gmail.com>
2013-07-31 14:07:24 -0500

    merging

--- tarificador/tarifica/views/users.py
+++ tarificador/tarifica/views/users.py
@@ -127,8 +127,55 @@ def detailUsers(request, extension_id, period_id="thisMonth"):
 
 
 
-def analiticsUsers(request, extension_id):
-    pass
+def analyticsUsers(request, extension_id, period_id="thisMonth"):
+    Ext = get_object_or_404(Extension, id = extension_id)
+    cursor = connection.cursor()
+    today = datetime.datetime.utcnow().replace(tzinfo=utc)
+    form = forms.getDate(initial=
+        {'start_date': datetime.date(year = today.year, month=today.month , day=1),
+         'end_date': datetime.date(year = today.year, month=today.month , day=today.day),
+        })
+    last_month = False
+    custom = False
+    end_date = datetime.date(year=today.year, month=today.month, day=today.day)
+    start_date = datetime.date(year=today.year, month=today.month, day=1)
+    start_date_year = datetime.date(year=today.year, month=1, day=1)
+    end_date_year = datetime.date(year=today.year, month=today.month, day=today.day)
+    if period_id == "lastMonth":
+        timedelta = datetime.timedelta(days = 1)
+        t = datetime.datetime(year=today.year, month=today.month , day=1)- timedelta
+        last_month = True
+        end_date = datetime.date(year=t.year, month=t.month, day=t.day)
+        start_date = datetime.date(year=t.year, month=t.month, day=1)
+    elif period_id == "custom":
+        if request.method == 'POST': # If the form has been submitted...
+            form = forms.getDate(request.POST) # A form bound to the POST data
+            if form.is_valid(): # All validation rules pass
+                start_date = form.cleaned_data['start_date']
+                end_date = form.cleaned_data['end_date']
+        custom = True
+    cursor.execute('SELECT tarifica_call.id, SUM(tarifica_call.cost) AS cost, tarifica_call.dialed_number,\
+        SUM(tarifica_call.duration) AS duration WHERE date > %s AND date < %s AND extension_number = %s \
+        GROUP BY dialed_number ORDER BY SUM(cost) DESC',
+        [start_date_year,end_date_year, Ext.extension_number])
+    top_calls = dictfetchall(cursor)[:10]
+    cursor.execute('SELECT tarifica_call.id, tarifica_call.cost, tarifica_call.dialed_number,\
+        tarifica_destinationname.name, tarifica_destinationcountry.name , tarifica_call.date AS dat,\
+        tarifica_call.date AS time FROM tarifica_call LEFT JOIN tarifica_destinationgroup\
+        ON tarifica_call.destination_group_id = tarifica_destinationgroup.id \
+        LEFT JOIN tarifica_destinationname ON tarifica_destinationgroup.destination_name_id = tarifica_destinationname.id \
+        LEFT JOIN tarifica_destinationcountry ON tarifica_destinationgroup.destination_country_id = tarifica_destinationcountry.id \
+        WHERE date > %s AND date < %s AND extension_number = %s',
+        [start_date,end_date, Ext.extension_number])
+    all_calls = dictfetchall(cursor)
+    return render(request, 'tarifica/analiticsUsers', {
+              'all_calls' : all_calls,
+              'top_calls' : top_calls,
+              'last_month' : last_month,
+              'custom' : custom,
+              'form': form,
+              'extension' : Ext,
+              })
 
 def dictfetchall(cursor):
     desc = cursor.description
