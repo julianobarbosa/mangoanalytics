35e2d87df6d9f1de4293d1e1a67bb5ee039edeb6
Alfonso Lizárraga <alfonso@nextortelecom.com>
2013-08-29 12:41:54 -0500

    welcome-screens

--- tarificador/tarifica/tools/assignCallCost.py
+++ tarificador/tarifica/tools/assignCallCost.py
@@ -1,5 +1,28 @@
 # -*- coding: utf-8 -*-
 
+#Licensed to NZXT TELECOMUNICACIONES DE MEXICO SA CV (Nextor Telecom) 
+#under GPL V3 license agreement.  
+#
+#See the NOTICE file distributed with this work for additional information
+#regarding copyright ownership. Nextor Telecom licenses this file
+#to you under the GPL V3 License, (the "License"); 
+#you may not use this file except in compliance
+#with the License.  You may obtain a copy of the License at
+#
+#http://opensource.org/licenses/GPL-3.0
+#
+#Unless required by applicable law or agreed to in writing,
+#software distributed under the License is distributed on an
+#"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#KIND, either express or implied.  See the License for the
+#specific language governing permissions and limitations
+#under the License.
+#
+#Nextor Telecom Mexico Dev-Ops dev@nextortelecom.com
+#
+#Alfonso Lizarraga Santos, Aug-2013
+#Luis Alfredo Lizarraga Santos, Aug-2013
+
 #!/usr/bin/env python2.7
 from __future__ import division
 from asteriskMySQLManager import AsteriskMySQLManager
