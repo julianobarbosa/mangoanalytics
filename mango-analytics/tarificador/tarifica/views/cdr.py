99886e1a73a1010279c31dec905a91f9147e4975
Alfonso Lizárraga <alfonso@nextortelecom.com>
2013-09-13 13:55:31 -0500

    whatev

---
+++ mango-analytics/tarificador/tarifica/views/cdr.py
@@ -0,0 +1,256 @@
+from __future__ import division
+import datetime
+from django.utils.timezone import utc
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseRedirect, HttpResponse, HttpResponseServerError
+from tarifica.tools.asteriskMySQLManager import AsteriskMySQLManager
+from tarifica.models import *
+from tarifica import forms
+from django.core.serializers.json import DjangoJSONEncoder
+from csv import *
+from math import ceil
+from django.db.models import Sum
+from tarifica.tools.elastix_session import elastix_user_is_authorized
+
+@elastix_user_is_authorized()
+def general(request, page=1):
+    user_info = get_object_or_404(UserInformation, id = 1)
+    action = "show"
+    calls = []
+    limit = 100
+    page = int(page) - 1    
+    # Query:
+    exclude_kwargs = {}
+    filter_kwargs = {}
+
+    if request.method == 'POST': # If the form has been submitted...
+        form = forms.filterCDR(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            #Delete all existing filters 
+            print 'Previous filters'
+            print CDRFilter.objects.all()
+            CDRFilter.objects.all().delete()
+            print 'After deleting filters'
+            print CDRFilter.objects.all()
+            for possible_filter in form.cleaned_data:
+                #If its the indicator of wether to show or download, ignore:
+                if possible_filter == 'action':
+                    action = form.cleaned_data[possible_filter]
+                    continue
+                #Checking if its a comparator
+                if possible_filter.count('comparison') == 0:
+
+                    field_name_val = possible_filter
+                    extras_val = ''
+                    if possible_filter == 'start_date' or possible_filter == 'end_date':
+                        field_name_val = 'date'
+                        if possible_filter == 'start_date':
+                            extras_val = 'start'
+                        if possible_filter == 'end_date':
+                            extras_val = 'end'
+                    if possible_filter == 'provider':
+                        #Check that we don't try to filter by id 0 (not selected)
+                        if form.cleaned_data[possible_filter] == '0':
+                            print "Provider not selected"
+                            continue
+                        field_name_val = 'provider_id'
+                    if possible_filter == 'destination_group':
+                        field_name_val = 'destination_group__destination_name__name'
+
+                    real_filter = CDRFilter()
+                    real_filter.comparison = form.cleaned_data[possible_filter+'_comparison']
+                    real_filter.field_name = field_name_val
+                    real_filter.value = form.cleaned_data[possible_filter]
+
+                    #Check start and end dates:
+                    if possible_filter == 'start_date' or possible_filter == 'end_date':
+                        if form.cleaned_data[possible_filter] is not None:
+                            real_filter.value = form.cleaned_data[possible_filter].strftime("%Y-%m-%d")
+                            if possible_filter == 'start_date':
+                                real_filter.extras = 'start'
+                            if possible_filter == 'end_date':
+                                real_filter.extras = 'end'
+                                        
+                    #Check that we only save as filters the fields that have a valid value
+                    if real_filter.value == '':
+                        continue
+                    if real_filter.value is None:
+                        continue 
+
+                    print "Field:", real_filter.field_name
+                    print "Comparison:", real_filter.comparison
+                    print "Value:", real_filter.value
+                    real_filter.save()
+                    print "Saved new filter"
+        else:
+            print "Not valid!"
+            print form.errors
+    else:
+        start_date_value = ''
+        start_date_comparison_value = ''
+        end_date_value = ''
+        end_date_comparison_value = ''
+        dialed_number_value = ''
+        dialed_number_comparison_value = ''
+        extension_number_value = ''
+        extension_number_comparison_value = ''
+        pinset_number_value = ''
+        pinset_number_comparison_value = ''
+        duration_value = ''
+        duration_comparison_value = ''
+        cost_value = ''
+        cost_comparison_value = ''
+        destination_group_value = ''
+        destination_group_comparison_value = ''
+        provider_value = ''
+        provider_comparison_value = ''
+
+        for f in CDRFilter.objects.all():
+            if f.field_name == 'date' and f.extras == 'start':
+                start_date_value = datetime.datetime.strptime(f.value, "%Y-%m-%d")
+                start_date_comparison_value = f.comparison
+            if f.field_name == 'date' and f.extras == 'end':
+                end_date_value = datetime.datetime.strptime(f.value, "%Y-%m-%d")
+                end_date_comparison_value = f.comparison
+            if f.field_name == 'dialed_number':
+                dialed_number_value = f.value 
+                dialed_number_comparison_value = f.comparison
+            if f.field_name == 'extension_number':
+                extension_number_value = f.value 
+                extension_number_comparison_value = f.comparison
+            if f.field_name == 'pinset_number':
+                pinset_number_value = f.value
+                pinset_number_comparison_value = f.comparison
+            if f.field_name == 'duration':
+                duration_value = f.value 
+                duration_comparison_value = f.comparison
+            if f.field_name == 'cost':
+                cost_value = f.value 
+                cost_comparison_value = f.comparison
+            if f.field_name == 'destination_group__destination_name__name':
+                destination_group_value = f.value 
+                destination_group_comparison_value = f.comparison
+            if f.field_name == 'provider__name':
+                provider_value = f.value 
+                provider_comparison_value = f.comparison
+
+        form = forms.filterCDR(initial={
+            'action': 'show',
+            'start_date': start_date_value,
+            'start_date_comparison': start_date_comparison_value,
+            'end_date': end_date_value,
+            'end_date_comparison': end_date_comparison_value,
+            'dialed_number': dialed_number_value,
+            'dialed_number_comparison': dialed_number_comparison_value,
+            'extension_number': extension_number_value,
+            'extension_number_comparison': extension_number_comparison_value,
+            'pinset_number': pinset_number_value,
+            'pinset_number_comparison': pinset_number_comparison_value,
+            'duration': duration_value,
+            'duration_comparison': duration_comparison_value,
+            'cost': cost_value,
+            'cost_comparison': cost_comparison_value,
+            'destination_group': destination_group_value,
+            'destination_group_comparison': destination_group_comparison_value,
+            'provider': provider_value,
+            'provider_comparison': provider_comparison_value,
+        }) # An unbound form
+
+    #Gettings saved filters
+    filters = CDRFilter.objects.all()
+    for f in filters:
+        if f.comparison == 'exclude':
+            exclude_kwargs.update(
+                {'{0}__{1}'.format(f.field_name, 'exact'): f.value}
+            )
+        else:
+            filter_kwargs.update(
+                {'{0}__{1}'.format(f.field_name, f.comparison): f.value}
+            )
+
+    if action == "download":
+        calls = Call.objects.filter(**filter_kwargs).exclude(**exclude_kwargs)
+        call_info = calls.values()
+        for call in call_info:
+            call['provider_id'] = Provider.objects.get(id=call['provider_id']).name
+            call['destination_group_id'] = DestinationGroup.objects.get(id=call['destination_group_id']).destination_name.name
+
+        file_path = '/tmp/'
+        file_name = 'custom_cdr.csv'
+
+        header = {
+            'date': 'Date',
+            'dialed_number': 'Dialed Number',
+            'extension_number': 'Extension Number',
+            'pinset_number': 'Pinset Number',
+            'provider_id': 'Provider',
+            'destination_group_id': 'Destination',
+            'duration': 'Minutes',
+            'cost': 'Cost',
+        }
+        field_names = [
+            'date',
+            'dialed_number',
+            'extension_number',
+            'pinset_number',
+            'provider_id',
+            'destination_group_id',
+            'duration',
+            'cost',
+        ]
+        try:
+            f = open(file_path + file_name, 'w+')
+            writer = DictWriter(f, fieldnames = field_names, extrasaction='ignore')
+            writer.writerow(header)
+            writer.writerows(call_info)
+            f.seek(0)
+        except Exception as e:
+            print "Error while writing file:",e
+            return HttpResponseServerError('Error while saving .csv:')
+
+        response = HttpResponse(f.read(), content_type='application/csv')
+        response['Content-Disposition'] = 'attachment; filename="'+file_name+'"'
+        f.close()
+        return response
+    else:
+        items = Call.objects.filter(**filter_kwargs).exclude(**exclude_kwargs).count()
+        calls = Call.objects.filter(**filter_kwargs).exclude(**exclude_kwargs)[page*limit:page*limit + limit]
+        total_seconds = Call.objects.filter(**filter_kwargs).exclude(**exclude_kwargs).aggregate(Sum('duration'))
+        total_cost = Call.objects.filter(**filter_kwargs).exclude(**exclude_kwargs).aggregate(Sum('cost'))
+
+    pages_number = int(ceil(items/limit))
+    previousPage = page - 1
+    if previousPage < 1:
+        previousPage = 1
+
+    nextPage = page + 1
+    if nextPage > pages_number:
+        nextPage = pages_number
+
+    if total_seconds['duration__sum'] is None:
+        total_seconds['duration__sum'] = 0
+
+    if total_cost['cost__sum'] is None:
+        total_cost['cost__sum'] = 0
+
+    return render(request, 'tarifica/general/cdr.html', {
+        'user_info' : user_info,
+        'calls' : calls,
+        'form': form,
+        'items': items,
+        'page': page + 1,
+        'pages': range(1, pages_number+1)[:10],
+        'limit': limit,
+        'previousPage': previousPage,
+        'nextPage': nextPage,
+        'pages_number': pages_number,
+        'total_minutes': total_seconds['duration__sum'] / 60,
+        'total_cost': total_cost['cost__sum'],
+    })
+
+def dictfetchall(cursor):
+    desc = cursor.description
+    return [
+        dict(zip([col[0] for col in desc], row))
+        for row in cursor.fetchall()
+    ]
---
+++ mango-analytics/tarificador/tarifica/views/wizard.py
@@ -0,0 +1,285 @@
+# Initial configuration views
+from __future__ import division
+import datetime
+from django.utils.timezone import utc
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from tarifica.tools.asteriskMySQLManager import AsteriskMySQLManager
+from tarifica.models import *
+from tarifica import forms
+from dateutil.relativedelta import *
+from math import ceil
+from envelopes import Envelope
+from tarifica.tools.elastix_session import elastix_user_is_authorized
+
+@elastix_user_is_authorized()
+def start(request, page=1):
+    user_info = get_object_or_404(UserInformation, id = 1)
+    user_info.first_time_user = False
+    user_info.save()
+
+    if page == '3':
+        if request.method == 'POST': # If the form has been submitted...
+            form = forms.getFirstUserInfo(request.POST)
+            if form.is_valid(): # All validation rules pass
+                user_info.country = form.cleaned_data['country']
+                user_info.bussiness_name = form.cleaned_data['bussiness_name']
+                user_info.contact_first_name = form.cleaned_data['contact_first_name']
+                user_info.contact_last_name = form.cleaned_data['contact_last_name']
+                user_info.notification_email = form.cleaned_data['notification_email']
+                user_info.currency_code = form.cleaned_data['currency_code']
+                user_info.currency_symbol = form.cleaned_data['currency_symbol']
+                user_info.accepted_privacy_policy = form.cleaned_data['accepted_privacy_policy']
+                if user_info.accepted_privacy_policy:
+                    user_info.first_time_user = False
+                    user_info.save()
+
+                    envelope = Envelope(
+                        from_addr = (
+                            user_info.notification_email, 
+                            user_info.contact_first_name + " " +
+                            user_info.contact_last_name
+                        ),
+                        to_addr = (
+                            'info@nextortelecom.com',
+                            u'To Nextor Telecom'
+                        ),
+                        subject = u'A new user has started using Mango Analytics!',
+                        text_body = "Name: {0} \nLast Name: {1} \nEmail: {2} \nCompany: {3} \
+                        \nCountry: {4} \n\n All hail Mango Analytics!".format(
+                            user_info.contact_first_name,
+                            user_info.contact_last_name,
+                            user_info.notification_email,
+                            user_info.bussiness_name,
+                            user_info.country.code
+                        )
+                    )
+
+                    try:
+                        envelope.send('localhost')
+                        print "Sent!"
+                    except Exception as e:
+                        #we could not send this mail...
+                        print "Could not send mail:",e
+
+                    return HttpResponseRedirect('/wizard/start/4') # Redirect after POST
+        else:
+            form = forms.getFirstUserInfo(initial={
+                'country': user_info.country,
+                'bussiness_name': user_info.bussiness_name,
+                'contact_first_name': user_info.contact_first_name,
+                'contact_last_name': user_info.contact_last_name,
+                'notification_email': user_info.notification_email,
+                'currency_code': user_info.currency_code,
+                'currency_symbol': user_info.currency_symbol,
+                'accepted_privacy_policy': user_info.accepted_privacy_policy
+            })
+
+        return render(request, 'tarifica/wizard/start3.html', {
+            'form': form
+        })
+
+    else:
+        user_info = get_object_or_404(UserInformation, id = 1)
+        referer = request.META.get('HTTP_REFERER')
+        return render(request, 'tarifica/wizard/start'+str(page)+'.html', {'referer':referer})
+
+@elastix_user_is_authorized()
+def testrun(request, default="none"):
+    import subprocess
+    import os
+
+    start_date = datetime.date.today()
+    start_date = start_date - relativedelta(months=6)
+
+    if request.method == 'POST': 
+        form = forms.getImportStartDate(request.POST) 
+        if form.is_valid():
+            start_date = form.cleaned_data['start_date']
+            default = 'default'
+    else:
+        form = forms.getImportStartDate(initial={
+            'start_date': start_date
+        })
+
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+
+    user_info = get_object_or_404(UserInformation, id = 1)
+    user_info.test_run_in_progress = True
+    user_info.save()
+
+    importer_script_path = current_directory+"/../tools/"
+    if default == 'default':
+        try:
+            p = subprocess.Popen([
+                'python2.7', 
+                importer_script_path+'importer.py', 
+                start_date.strftime("%Y-%m-%d"), 
+                '--testrun'
+            ])
+            # p = subprocess.check_output(['python2.7', importer_script_path+'importer.py', '--testrun'])
+        except Exception, e:
+            print "Error while dry running:",e
+            p = None
+        user_info.first_import_started = datetime.datetime.now()
+        user_info.save()
+        return HttpResponseRedirect('/wizard/checkTestRunStatus/show')
+
+    return render(request, 'tarifica/wizard/testRun.html', {
+        'form': form, 
+        'start_date': start_date,
+        'default': default
+    })
+
+@elastix_user_is_authorized()
+def checkTestRunStatus(request, action="show"):
+    user_info = get_object_or_404(UserInformation, id = 1)
+    # By default, we import 6 month's worth of data, so we can check how many
+    # days are between today and the last date imported.
+    try:
+        now = datetime.datetime.now()
+        lastCall = Call.objects.latest('date')
+        lapsedTime = now - user_info.first_import_started
+        lapsedSeconds = lapsedTime.total_seconds()
+        lapsedMinutes = ceil(lapsedTime.total_seconds() / 60)
+
+        endDateForProcessing = datetime.date(
+            year = user_info.first_import_started.year,
+            month = user_info.first_import_started.month,
+            day = user_info.first_import_started.day
+        )
+        initialDateForProcessing = endDateForProcessing - datetime.timedelta(days = 141)
+
+        daysToBeProcessed = 140
+        processedDays = (lastCall.date.date() - initialDateForProcessing).days
+        percentage_imported = ( processedDays / daysToBeProcessed ) * 100
+        totalMinutes = ceil(( daysToBeProcessed * lapsedMinutes ) / processedDays)
+        minutesRemaining = totalMinutes - lapsedMinutes
+        
+    except ObjectDoesNotExist, e:
+        percentage_imported = 0
+        lapsedMinutes = 0
+        minutesRemaining = 'infinite'
+
+    if action == 'update':
+        # AJAX request to show info
+        return render(request, 'tarifica/wizard/updateTestRunStatus.html', {
+            'user_info': user_info,
+            'percentage_imported': percentage_imported,
+            'lapsedMinutes': lapsedMinutes,
+            'minutesRemaining': minutesRemaining
+        })
+
+    return render(request, 'tarifica/wizard/checkTestRunStatus.html', {
+        'user_info': user_info,
+        'percentage_imported': percentage_imported,
+        'lapsedMinutes': lapsedMinutes,
+        'minutesRemaining': minutesRemaining
+    })
+
+@elastix_user_is_authorized()
+def results(request):
+    user_info = get_object_or_404(UserInformation, id = 1)
+    user_info.is_first_import_finished = False
+    user_info.save()
+
+    import_results = ImportResults.objects.order_by('-id')[0]
+    unconfigured_calls = UnconfiguredCall.objects.all()[:100]
+    return render(request, 'tarifica/wizard/results.html', {
+        'import_results': import_results,
+        'unconfigured_calls': unconfigured_calls,
+        'percentage_not_processed': (import_results.calls_not_saved / (import_results.calls_not_saved + import_results.calls_saved)) * 100
+    })
+
+@elastix_user_is_authorized()
+def run(request, default="none"):
+    import subprocess
+    import os
+
+    start_date = datetime.date.today()
+    start_date = start_date - relativedelta(months=6)
+
+    if request.method == 'POST': 
+        form = forms.getImportStartDate(request.POST) 
+        if form.is_valid():
+            start_date = form.cleaned_data['start_date']
+            default = 'default'
+    else:
+        form = forms.getImportStartDate(initial={
+            'start_date': start_date
+        })
+
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+
+    user_info = get_object_or_404(UserInformation, id = 1)
+    user_info.processing_in_progress = True
+    user_info.save()
+    importer_script_path = current_directory+"/../tools/"
+    if default == 'default':
+        try:
+            p = subprocess.Popen([
+                'python2.7', 
+                importer_script_path+'importer.py',
+                start_date.strftime("%Y-%m-%d")
+            ])
+            # p = subprocess.check_output(['python2.7', importer_script_path+'importer.py'])
+        except Exception, e:
+            print "Error while digesting:",e
+            p = None
+        user_info.first_import_started = datetime.datetime.now()
+        user_info.is_first_import_finished = False
+        user_info.save()
+        return HttpResponseRedirect('/wizard/checkProcessingStatus/show')
+
+    return render(request, 'tarifica/wizard/run.html', {
+        'form': form, 
+        'start_date': start_date,
+        'default': default
+    })
+
+@elastix_user_is_authorized()
+def checkProcessingStatus(request, action="show"):
+    user_info = get_object_or_404(UserInformation, id = 1)
+    # By default, we import 6 month's worth of data, so we can check how many
+    # days are between today and the last date imported.
+    try:
+        now = datetime.datetime.now()
+        lastProviderDailyDetail = ProviderDailyDetail.objects.latest('date')
+        lapsedTime = now - user_info.first_import_started
+        lapsedSeconds = lapsedTime.total_seconds()
+        lapsedMinutes = lapsedTime.total_seconds() / 60
+
+        endDateForProcessing = datetime.date(
+            year = user_info.first_import_started.year,
+            month = user_info.first_import_started.month,
+            day = user_info.first_import_started.day
+        )
+        initialDateForProcessing = endDateForProcessing - datetime.timedelta(days = 141)
+
+        daysToBeProcessed = 140
+        processedDays = (lastProviderDailyDetail.date - initialDateForProcessing).days
+        percentage_imported = ( processedDays / daysToBeProcessed ) * 100
+        totalMinutes = ( daysToBeProcessed * lapsedMinutes ) / processedDays
+        minutesRemaining = totalMinutes - lapsedMinutes
+        
+    except ObjectDoesNotExist, e:
+        percentage_imported = 0
+        lapsedMinutes = 0
+        minutesRemaining = 'infinite'
+
+    if action == 'update':
+        # AJAX request to show info
+        return render(request, 'tarifica/wizard/updateProcessingStatus.html', {
+            'user_info': user_info,
+            'percentage_imported': percentage_imported,
+            'lapsedMinutes': lapsedMinutes,
+            'minutesRemaining': minutesRemaining
+        })
+
+    return render(request, 'tarifica/wizard/checkProcessingStatus.html', {
+        'user_info': user_info,
+        'percentage_imported': percentage_imported,
+        'lapsedMinutes': lapsedMinutes,
+        'minutesRemaining': minutesRemaining
+    })
