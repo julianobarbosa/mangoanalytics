f8bfd0d137a0d6159f289603d70d849c7437810d
Alfonso Lizárraga <alfonso@nextortelecom.com>
2013-12-12 16:25:50 -0600

    inicializando repo privado con versión 1.0.5 del repo público

--- tarificador/tarifica/views/users.py
+++ tarificador/tarifica/views/users.py
@@ -501,8 +501,6 @@ def getBarChartInfoByExtForYear(cursor, extension_id):
         else:
             aux.append([month, monthCost[0]['cost']])
         #print aux
-        if fDate.month == 12:
-            break
         sDate = datetime.datetime(day=1, month=fDate.month+1, year=fDate.year).replace(tzinfo=utc)
     data.append(aux)
     return data
